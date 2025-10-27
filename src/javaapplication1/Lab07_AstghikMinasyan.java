59bbba7f2b43a5dbcec8a32e9fe577c33c644711
Astghik-hub <minastghik@gmail.com>
2025-10-27 01:14:38 -0400

    added comments and formatted

--- src/javaapplication1/Lab07_AstghikMinasyan.java
+++ src/javaapplication1/Lab07_AstghikMinasyan.java
@@ -28,6 +28,7 @@ import javafx.util.Duration;
 
 /**
  * Git URL: https://github.com/Astghik-hub/Lab07_AstghikMinasyan.git
+ *
  * @author Astghik Minasyan
  */
 public class Lab07_AstghikMinasyan extends Application {
@@ -40,26 +41,31 @@ public class Lab07_AstghikMinasyan extends Application {
     }
 
     @Override
-    public void start(Stage stage) { 
+    public void start(Stage stage) {
+        // Create pane and scene
         Pane pane = new Pane();
-        Scene scene = new Scene(pane, 700,700);
-        
+        Scene scene = new Scene(pane, 700, 700);
+
+        // Create object A
+        Circle circle = new Circle(80, 80, 50);
+
+        // Create path so that object B will go clockwise
         Path path = new Path();
         path.getElements().add(new MoveTo(80, 80));
         path.getElements().add(new LineTo(80 + 470, 80));
         path.getElements().add(new LineTo(80 + 470, 80 + 470));
         path.getElements().add(new LineTo(80, 80 + 470));
         path.getElements().add(new LineTo(80, 80));
-        
-        Circle circle = new Circle(80, 80, 50);
-        
+
+        // Create object A
         Ellipse oval = new Ellipse();
         oval.setRadiusX(40);
         oval.setRadiusY(25);
         oval.setCenterX(355);
         oval.setCenterY(285);
         oval.setFill(Color.BLUE);
-        
+
+        // Create start, reset and exit buttons
         Button start = new Button("Start");
         start.setLayoutX(60);
         start.setLayoutY(610);
@@ -69,45 +75,61 @@ public class Lab07_AstghikMinasyan extends Application {
         Button exit = new Button("Exit");
         exit.setLayoutX(180);
         exit.setLayoutY(610);
-       
+
+        // Add the elements on the pane
         pane.getChildren().add(circle);
         pane.getChildren().add(oval);
         pane.getChildren().add(start);
         pane.getChildren().add(reset);
         pane.getChildren().add(exit);
-        
-        PathTransition pt  = new PathTransition(Duration.millis(5000), path, circle);
+
+        // Create a path transition
+        PathTransition pt = new PathTransition(Duration.millis(5000), path, circle);
         pt.setCycleCount(Timeline.INDEFINITE);
-        
+
+        // Create a fade transition
         FadeTransition fade = new FadeTransition(Duration.millis(1400), oval);
         fade.setFromValue(1.0);
         fade.setToValue(0.3);
-        
+
+        // Create a scale transition
         ScaleTransition scale = new ScaleTransition(Duration.millis(1100), oval);
         scale.setToX(2);
         scale.setToY(2);
-        
+
+        // Create a rotate transition
         RotateTransition rotate = new RotateTransition(Duration.millis(1000), oval);
         rotate.setFromAngle(0);
         rotate.setToAngle(90);
-        
+
+        // Create a translate transtion
         TranslateTransition translate = new TranslateTransition(Duration.millis(1500), oval);
         translate.setToX(290);
-        
+
+        // Create sequential transition
+        // Add fade, scale, rotate and translate to sequential transition
         SequentialTransition sequence = new SequentialTransition(fade, scale, rotate, translate);
         sequence.setCycleCount(Animation.INDEFINITE);
-        
+
+        // Create parallel transtition
+        // Add pt and sequence to it
         ParallelTransition both = new ParallelTransition(pt, sequence);
-        
-        start.setOnMouseClicked(e ->  both.play());
+
+        // Play both when clicking start
+        start.setOnMouseClicked(e -> both.play());
+
+        // Reset animation to original form when clicking reset
         reset.setOnMouseClicked(e -> {
             both.playFrom(Duration.ZERO);
             both.pause();
-                });
+        });
+
+        // Exit the animation when clicking exit
         exit.setOnMouseClicked(e -> stage.close());
-        
+
+        // Set the scene of the stage and show it
         stage.setScene(scene);
         stage.show();
     }
-    
+
 }
